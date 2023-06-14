1457bc5471098cc788a77cbfb60117b916568f61
debsouryadatta <debsouryadatta@gmail.com>
2023-06-14 17:50:04 +0530

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
